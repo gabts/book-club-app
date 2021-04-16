9fde99f161d324d25c89ba207646f727a93d343d
Gabriel Toll Stålbom <gabtost@gmail.com>
2021-04-16 21:28:58 +0200

    add database ssl if production

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,6 +3,7 @@ import type * as types from "./types";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production",
 });
 
 export async function addBook(input: types.BookInput) {
